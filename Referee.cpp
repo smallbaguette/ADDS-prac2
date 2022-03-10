1238a4264528e3ad3cfc254db876896c64a5cb8a
smallbaguette <angelwong712002@gmail.com>
2022-03-10 22:08:12 +1030

    PLEEEASE

--- Referee.cpp
+++ Referee.cpp
@@ -8,11 +8,11 @@ Referee::Referee(){
 }
 
 char Referee::refGame(Human player1, Computer player2) {
-	if (player1.move=='P') {
-	    return 'W';
-	} else if (player1.move=='S') {
-	    return 'L';
-	} else if (player1.move=='R') {
-	    return 'T';
+	if (player1.move=='P' && player2.move=='R') {
+		return 'W';
+	} else if (player1.move=='S' && player2.move=='R') {
+		return 'L';
+	} else if (player1.move=='R' && player2.move=='R') {
+		return 'T';
 	}
 }
